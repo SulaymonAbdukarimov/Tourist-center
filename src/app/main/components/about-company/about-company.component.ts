9b649ad8dcd9accb449741232c9dbd5488ac1efe
SulaymonAbdukarimov <sulaymonabdukarimov2@gmail.com>
2024-07-16 01:27:38 +0500

    navbar,hero and accardion have done

---
+++ src/app/main/components/about-company/about-company.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-about-company',
+  templateUrl: './about-company.component.html',
+  styleUrls: ['./about-company.component.css'],
+  standalone: true
+})
+export class AboutCompanyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
