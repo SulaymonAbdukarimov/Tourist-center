43eac8f4c27a9253404b33a9ca076df7daa2caea
Rikhsi <omonbek.rikhsimboev@eventplatform.ru>
2024-07-24 04:55:25 +0500

    fixes

--- src/app/main/components/contact-us/contact-us.component.ts
+++ src/app/main/components/contact-us/contact-us.component.ts
@@ -1,18 +1,24 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { NgxMaskDirective } from 'ngx-mask';
 import emailjs from 'emailjs-com';
+import { NzMessageService } from 'ng-zorro-antd/message';
+import { NzSpinModule } from 'ng-zorro-antd/spin';
 
 @Component({
   standalone: true,
-  imports: [ReactiveFormsModule, NgxMaskDirective],
+  imports: [ReactiveFormsModule, NgxMaskDirective, NzSpinModule],
   selector: 'app-contact-us',
   templateUrl: './contact-us.component.html',
   styleUrls: ['./contact-us.component.scss'],
+  providers: [NzMessageService]
 })
 export class ContactUsComponent implements OnInit {
   private $formBuilder = inject(FormBuilder);
-  constructor() {}
+  isLoading = signal<boolean>(false);
+
+  constructor(private messageService: NzMessageService) {}
+
   userForm = this.$formBuilder.group({
     name: ['', Validators.required],
     phone: ['', Validators.required],
@@ -23,7 +29,9 @@ export class ContactUsComponent implements OnInit {
   ngOnInit() {}
 
   submit(): void {
-    if (this.userForm.valid) {
+    if (this.userForm.valid && !this.isLoading()) {
+      this.isLoading.set(true);
+
       emailjs
         .send(
           'service_mt0biyn',
@@ -31,8 +39,17 @@ export class ContactUsComponent implements OnInit {
           this.userForm.value,
           'KS8xO3kZD-UCobEDo'
         )
-        .then(() => {})
-        .catch(() => {});
+        .then(() => {
+          this.isLoading.set(false);
+          this.messageService.success('Ваш запрос был успешно отправлен.');
+          this.userForm.reset();
+        })
+        .catch(() => {
+          this.isLoading.set(false);
+          this.messageService.error('Произошла ошибка, пожалуйста, повторите позже.');
+        });
+    } else {
+      this.messageService.error('Пожалуйста, заполните все поля.');
     }
   }
 }
